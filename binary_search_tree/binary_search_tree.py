93a61385188bdfabdcd33e844189e1c2e11a357d
Eddie Madrigal <edmadrigal@yahoo.com>
2020-07-16 18:05:39 -0700

    files refactored

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,9 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+from queue import Queue
+from stack import Stack
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -16,17 +19,20 @@ class BSTNode:
         self.right = None
 
     # Insert the given value into the tree
-    def insert(self, value):                            
-        if value < self.value:                      # if value is less than self.value                        
-            if self.left is None:                   # no left child exists
-                self.left = BSTNode(value)          # make the left child node be an instance of BSTNode with value
-            else:                                   # a left child exists
-                self.left.insert(value)             # recurse until there are no left childs exist
-        elif value >= self.value:                   # else if the value coming in is greater than existing self.value
-            if self.right is None:                  # if self.right (right child) does not exist
-                self.right = BSTNode(value)         # run the value through BSTNode class and assign it to self's right child
-            else:                                   # a self.right child node exists
-                self.right = insert(value)          # retry the insert(value) function until there are no child nodes left
+    def insert(self, value):
+        new_node = BSTNode(value)                            
+        if not self.value: 
+            self.value = value        
+        elif value < self.value:
+            if not self.left:
+                self.left = new_node
+            else:
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = new_node
+            else:
+                self.right.insert(value)             
 
     # Return True if the tree contains the value
     # False if it does not
@@ -34,43 +40,69 @@ class BSTNode:
         if self.value == target:                    # if the self.value equals target
             return True                             # return True
         if target < self.value:                     # if the target we're searching for is less than self.value
-            if self.left is None:                   # no left child exists
+            if not self.left:                   # no left child exists
                 return False                        # return false
             else:                                   # a left child exists
                 return self.left.contains(target)   # try again, only start the search from here    
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
             
 
     # Return the maximum value found in the tree
     def get_max(self):                              # get_max function definition
-        if self.value < self.right:                 # if self.value is less than the right child tree node
-            self.value = self.right                 # set the self.value to check for equal the right child tree node
-            return self.right.get_max(self.value)   # re-run the function, only this time, start with the current self.value
+        if not self.right:                 # if self.value is less than the right child tree node
+            return self.value
         else:                                       # there is no right value
-            return self.value                       # return the self.value (the self.value is the max value by default)
+            return self.right.get_max()                     # return the self.value (the self.value is the max value by default)
         
     # Call the function `fn` on the value of each node
     def for_each(self, fn):                         # for_each node found, run a function called fn on the value i.e., fn(self.value)
         fn(self.value)                              # fn(self.value) is run on each node's value 
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self):
-        print()
-        if self.value is not None:
-            print('In order: ')
-            self.value.in_order_print()
+    def in_order_print(self, node):
+        if node:
+            self.in_order_print(node.left)
+            print(node.value)
+            self.in_order_print(node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        queue = queue.Queue()
+        queue.put(node)
+
+        while queue.qsize() > 0:
+            current_node = queue.get()
+            print(current_node.value)
+            if current_node.left:
+                queue.put(current_node.left)
+            if current_node.right:
+                queue.put(current_node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = stack.Stack()
+        stack.push(node)
+        while len(stack) > 0:
+            current_node = stack.pop()
+            print(current_node.value)
+            if current_node.left:
+                stack.push(current_node.left)
+            if current_node.right:
+                stack.push(current_node.right)
+
 
     # Stretch Goals -------------------------
     # Note: Research may be required
@@ -82,8 +114,3 @@ class BSTNode:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
-
-
-r = BSTNode(50)
-r.insert(10)
-r.in_order_print()
--- binary_search_tree/queue.py
+++ binary_search_tree/queue.py
@@ -13,10 +13,6 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
-import sys
-sys.path.append('/Lambda/git/Data-Structures/queue_and_stack')
-from queue import Queue
-from stack import Stack
 class Queue:
     def __init__(self):
         self.size = 0
